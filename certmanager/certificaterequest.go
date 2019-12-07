70763740c2307a8fe077c2f757a8713143ee2ec1
Tamal Saha <tamal@appscode.com>
2019-12-07 09:42:41 -0800

    Add certmanager patch utils
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ certmanager/certificaterequest.go
@@ -0,0 +1,142 @@
+/*
+Copyright The Kmodules Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package certmanager
+
+import (
+	"encoding/json"
+	"fmt"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/golang/glog"
+	api "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha2"
+	cs "github.com/jetstack/cert-manager/pkg/client/clientset/versioned/typed/certmanager/v1alpha2"
+	"github.com/pkg/errors"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	kutil "kmodules.xyz/client-go"
+)
+
+func CreateOrPatchCertificateRequest(c cs.CertmanagerV1alpha2Interface, meta metav1.ObjectMeta, transform func(alert *api.CertificateRequest) *api.CertificateRequest) (*api.CertificateRequest, kutil.VerbType, error) {
+	cur, err := c.CertificateRequests(meta.Namespace).Get(meta.Name, metav1.GetOptions{})
+	if kerr.IsNotFound(err) {
+		glog.V(3).Infof("Creating CertificateRequest %s/%s.", meta.Namespace, meta.Name)
+		out, err := c.CertificateRequests(meta.Namespace).Create(transform(&api.CertificateRequest{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "CertificateRequest",
+				APIVersion: api.SchemeGroupVersion.String(),
+			},
+			ObjectMeta: meta,
+		}))
+		return out, kutil.VerbCreated, err
+	} else if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+	return PatchCertificateRequest(c, cur, transform)
+}
+
+func PatchCertificateRequest(c cs.CertmanagerV1alpha2Interface, cur *api.CertificateRequest, transform func(*api.CertificateRequest) *api.CertificateRequest) (*api.CertificateRequest, kutil.VerbType, error) {
+	return PatchCertificateRequestObject(c, cur, transform(cur.DeepCopy()))
+}
+
+func PatchCertificateRequestObject(c cs.CertmanagerV1alpha2Interface, cur, mod *api.CertificateRequest) (*api.CertificateRequest, kutil.VerbType, error) {
+	curJson, err := json.Marshal(cur)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+
+	modJson, err := json.Marshal(mod)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+
+	patch, err := jsonpatch.CreateMergePatch(curJson, modJson)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+	if len(patch) == 0 || string(patch) == "{}" {
+		return cur, kutil.VerbUnchanged, nil
+	}
+	glog.V(3).Infof("Patching CertificateRequest %s/%s with %s.", cur.Namespace, cur.Name, string(patch))
+	out, err := c.CertificateRequests(cur.Namespace).Patch(cur.Name, types.MergePatchType, patch)
+	return out, kutil.VerbPatched, err
+}
+
+func TryUpdateCertificateRequest(c cs.CertmanagerV1alpha2Interface, meta metav1.ObjectMeta, transform func(*api.CertificateRequest) *api.CertificateRequest) (result *api.CertificateRequest, err error) {
+	attempt := 0
+	err = wait.PollImmediate(kutil.RetryInterval, kutil.RetryTimeout, func() (bool, error) {
+		attempt++
+		cur, e2 := c.CertificateRequests(meta.Namespace).Get(meta.Name, metav1.GetOptions{})
+		if kerr.IsNotFound(e2) {
+			return false, e2
+		} else if e2 == nil {
+			result, e2 = c.CertificateRequests(cur.Namespace).Update(transform(cur.DeepCopy()))
+			return e2 == nil, nil
+		}
+		glog.Errorf("Attempt %d failed to update CertificateRequest %s/%s due to %v.", attempt, cur.Namespace, cur.Name, e2)
+		return false, nil
+	})
+
+	if err != nil {
+		err = errors.Errorf("failed to update CertificateRequest %s/%s after %d attempts due to %v", meta.Namespace, meta.Name, attempt, err)
+	}
+	return
+}
+
+func UpdateCertificateRequestStatus(
+	c cs.CertmanagerV1alpha2Interface,
+	in *api.CertificateRequest,
+	transform func(*api.CertificateRequestStatus) *api.CertificateRequestStatus,
+) (result *api.CertificateRequest, err error) {
+	apply := func(x *api.CertificateRequest) *api.CertificateRequest {
+		return &api.CertificateRequest{
+			TypeMeta:   x.TypeMeta,
+			ObjectMeta: x.ObjectMeta,
+			Spec:       x.Spec,
+			Status:     *transform(in.Status.DeepCopy()),
+		}
+	}
+
+	attempt := 0
+	cur := in.DeepCopy()
+	err = wait.PollImmediate(kutil.RetryInterval, kutil.RetryTimeout, func() (bool, error) {
+		attempt++
+		var e2 error
+		result, e2 = c.CertificateRequests(in.Namespace).UpdateStatus(apply(cur))
+		if kerr.IsConflict(e2) {
+			latest, e3 := c.CertificateRequests(in.Namespace).Get(in.Name, metav1.GetOptions{})
+			switch {
+			case e3 == nil:
+				cur = latest
+				return false, nil
+			case kutil.IsRequestRetryable(e3):
+				return false, nil
+			default:
+				return false, e3
+			}
+		} else if err != nil && !kutil.IsRequestRetryable(e2) {
+			return false, e2
+		}
+		return e2 == nil, nil
+	})
+
+	if err != nil {
+		err = fmt.Errorf("failed to update status of CertificateRequest %s/%s after %d attempts due to %v", in.Namespace, in.Name, attempt, err)
+	}
+	return
+}
---
+++ certmanager/clusterissuer.go
@@ -0,0 +1,98 @@
+/*
+Copyright The Kmodules Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package certmanager
+
+import (
+	"encoding/json"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/golang/glog"
+	api "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha2"
+	cs "github.com/jetstack/cert-manager/pkg/client/clientset/versioned/typed/certmanager/v1alpha2"
+	"github.com/pkg/errors"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	kutil "kmodules.xyz/client-go"
+)
+
+func CreateOrPatchClusterIssuer(c cs.CertmanagerV1alpha2Interface, meta metav1.ObjectMeta, transform func(*api.ClusterIssuer) *api.ClusterIssuer) (*api.ClusterIssuer, kutil.VerbType, error) {
+	cur, err := c.ClusterIssuers().Get(meta.Name, metav1.GetOptions{})
+	if kerr.IsNotFound(err) {
+		glog.V(3).Infof("Creating ClusterIssuer %s", meta.Name)
+		out, err := c.ClusterIssuers().Create(transform(&api.ClusterIssuer{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "ClusterIssuer",
+				APIVersion: api.SchemeGroupVersion.String(),
+			},
+			ObjectMeta: meta,
+		}))
+		return out, kutil.VerbCreated, err
+	} else if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+	return PatchClusterIssuer(c, cur, transform)
+}
+
+func PatchClusterIssuer(c cs.CertmanagerV1alpha2Interface, cur *api.ClusterIssuer, transform func(*api.ClusterIssuer) *api.ClusterIssuer) (*api.ClusterIssuer, kutil.VerbType, error) {
+	return PatchClusterIssuerObject(c, cur, transform(cur.DeepCopy()))
+}
+
+func PatchClusterIssuerObject(c cs.CertmanagerV1alpha2Interface, cur, mod *api.ClusterIssuer) (*api.ClusterIssuer, kutil.VerbType, error) {
+	curJson, err := json.Marshal(cur)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+
+	modJson, err := json.Marshal(mod)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+
+	patch, err := jsonpatch.CreateMergePatch(curJson, modJson)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+	if len(patch) == 0 || string(patch) == "{}" {
+		return cur, kutil.VerbUnchanged, nil
+	}
+	glog.V(3).Infof("Patching ClusterIssuer %s with %s.", cur.Name, string(patch))
+	out, err := c.ClusterIssuers().Patch(cur.Name, types.MergePatchType, patch)
+	return out, kutil.VerbPatched, err
+}
+
+func TryUpdateClusterIssuer(c cs.CertmanagerV1alpha2Interface, meta metav1.ObjectMeta, transform func(*api.ClusterIssuer) *api.ClusterIssuer) (result *api.ClusterIssuer, err error) {
+	attempt := 0
+	err = wait.PollImmediate(kutil.RetryInterval, kutil.RetryTimeout, func() (bool, error) {
+		attempt++
+		cur, e2 := c.ClusterIssuers().Get(meta.Name, metav1.GetOptions{})
+		if kerr.IsNotFound(e2) {
+			return false, e2
+		} else if e2 == nil {
+			result, e2 = c.ClusterIssuers().Update(transform(cur.DeepCopy()))
+			return e2 == nil, nil
+		}
+		glog.Errorf("Attempt %d failed to update ClusterIssuer %s due to %v.", attempt, cur.Name, e2)
+		return false, nil
+	})
+
+	if err != nil {
+		err = errors.Errorf("failed to update ClusterIssuer %s after %d attempts due to %v", meta.Name, attempt, err)
+	}
+	return
+}
