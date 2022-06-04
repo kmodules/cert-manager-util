1448d5d0afe88765b9f7f596409f550049be1eaa
1gtm <1gtm@appscode.com>
2022-06-04 05:07:15 +0000

    Test against Kubernetes 1.24.0
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- certmanager/v1/clusterissuer.go
+++ certmanager/v1/clusterissuer.go
@@ -20,9 +20,9 @@ import (
 	"context"
 	"encoding/json"
 
-	jsonpatch "github.com/evanphx/json-patch"
 	api "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	cs "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned/typed/certmanager/v1"
+	jsonpatch "github.com/evanphx/json-patch"
 	"github.com/pkg/errors"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
